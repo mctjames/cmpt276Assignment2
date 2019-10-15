2a9232dbfdba801f00579304a4fb440b20c09f83
mctjames <mctaggartjames@gmail.com>
2019-10-15 16:31:49 -0700

    new attempt

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+//const port = normalizePort(process.env.PORT || '5000');
+const port = 5000;
+const path = require('path')
+app.set('view engine', 'ejs');
+
+//body parser middleware
+app.use(bodyParser.json())
+ // app.use(bodyParser.urlencoded({extended: true,}))
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+
+//give path information to access the files in the piblic folder - the css and js stuff
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname +'/public/home.html'));
+  //response.json({ info: 'Node.js, Express, and Postgres API' })
+});
+
+
+//more testing 
+
+var mongoose = require('mongoose');
+
+//connect to mongoose database
+mongoose.connect("mongodb+srv://james:password12345@cluster0-mfucr.mongodb.net/test?retryWrites=true&w=majority", 
+  {useUnifiedTopology:true, useNewUrlParser:true, useCreateIndex: true});
+
+//create a schema - the blueprint for our data
+var tokiSchema = new mongoose.Schema({
+  name: String,
+  flying: Number,
+  fighting: Number,
+  fire: Number,
+  water:Number,
+  electric:Number,
+  frozen:Number,
+  total: Number,
+  weight:Number,
+  height:Number,
+  trainer:String,
+});
+// created a modeled based on the above schema
+var Tokimon = mongoose.model('Tokimon', tokiSchema);
+
+
+
+app.get('/todo', function(req, res){
+  //get data from mongodb and pass to view
+  //finds all the items in the collextion/database
+  // to get a specific item .find({item: 'specific identifier'})
+  Tokimon.find({}, function(err, data){
+    if(err) throw err;
+    res.render('todo', {tokimon: data});
+  });
+});
+
+
+app.post('/todo', urlencodedParser, function(req,res){
+  //get data from the view and add it to mongodb
+  var newTodo = Tokimon(req.body).save(function(err, data){
+    if(err) throw err;
+    res.json(data);
+  });
+});
+
+
+
+app.delete('/todo/:name', function(req,res){
+  // delete the requested item from mongo
+  Tokimon.find({name: req.params.name.replace(/\-/g, " ")}).remove(function(err, data){
+    if(err) throw err;
+    res.json(data);
+  });
+});
+
+
+
+//server 
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
+
+
+
---
+++ public/assets/todo-list.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+  $('form').on('submit', function(){
+
+      var name = $('form input');
+      var todo = {name: name.val()};
+      // var flying = $('form');
+      // var statOne = {flying: flying.val()};
+
+
+      $.ajax({
+        type: 'POST',
+        url: '/todo',
+        data: todo, //statOne,
+        success: function(data){
+          //do something with the data via front-end framework
+          location.reload();
+        }
+      });
+
+      return false;
+
+  });
+
+
+  $('li').on('click', function(){
+      var name = $(this).text().replace(/ /g, "-");
+      $.ajax({
+        type: 'DELETE',
+        url: '/todo/' + name,
+        success: function(data){
+          location.reload();
+        }
+      });
+  });
+
+});
